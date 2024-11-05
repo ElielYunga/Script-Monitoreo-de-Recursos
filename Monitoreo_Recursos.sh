3e841c5f5708d893f6c68b347c238ffc13c8ce9a
edissoncuenca <xavier.cuenca7@ucuenca.edu.ec>
2024-11-04 23:16:04 -0500

    Primer commit

---
+++ Monitoreo_Recursos.sh
@@ -0,0 +1,21 @@
+#Encabezado
+echo "Tiempo, %Total CPU libre, %Memoria Libre, %Disco Libre"
+
+for i in {1..5}
+do
+#Tiempo en segundo
+ tiempo=$(($i * 60))
+
+#Calculo del uso de cpu 
+ cpu=$(top -bn1 | grep "Cpu(s)" | awk '{print 100 - $2}')
+
+#Calculo del uso de Ram
+ memoria=$(free | grep Mem | awk '{print $4/$2 * 100}' )
+
+#Calculo uso del disco
+ disco=$(df -h / | grep '^/' | awk '{print $5}' | sed 's/%//')
+
+ echo "$tiempo, $cpu, $memoria, $disco"
+
+ sleep 60
+done
